5343139637a5f90958f379b5b7dba852ab64aed3
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 13:29:36 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end 
   
   def list_roles 
-    self.characters.map {|c| "#{c.name} - #{c.show.name}"
+    self.characters.map {|c| "#{c.name} - #{c.show.name}"}
   end 
   
 end
